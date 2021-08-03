d9bc1e6395c4357c74c5fc6dc0cf3d32fbb0227e
Jennifer Law <jenniferlaw266@gmail.com>
2021-08-02 17:32:17 -0700

    Included LoopyLighthouse

--- chorus-for.js
+++ chorus-for.js
@@ -3,3 +3,16 @@ for (let repeat = 0; repeat < 10; repeat++) {
   console.log(chorus);
 }
 console.log("Until the sun comes up!");
+
+//Loopy Lighthouse
+for(let i=100;i<201;i++){ 
+  if(i%3===0 && i%4===0){ 
+    console.log("LoopyLighthouse");
+  } else if(i%4===0){ 
+    console.log("Lighthouse");
+  } else if(i%3===0){ 
+    console.log("Loopy");
+  } else{ 
+    console.log(i);
+  }
+}

f190cf0d89e72bff887536d3a32ad9e713fc866b
Jennifer Law <jenniferlaw266@gmail.com>
2021-08-02 17:02:05 -0700

    Included whichSchool exercise

--- conditionals.js
+++ conditionals.js
@@ -56,3 +56,20 @@ if (!raining) {
 
 
 
+
+
+//whichSchool
+/*Elementary School if age is below 13
+Secondary School if age is between 13 and 18 (both inclusive)
+Lighthouse Labs in all other cases.
+*/
+const whichSchool  = function (age) {
+  if (age<13){
+    return "Elementary School";
+  } else if (age=>13 && age <=18) {
+    return "Secondary School";
+  } else{
+    return "Lighthouse Labs";
+  }
+}
+  console.log(whichSchool(18)); 

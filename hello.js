496bb814c7d73e988598eb0f9647fcbe17018b3b
Jennifer Law <jenniferlaw266@gmail.com>
2021-08-03 16:34:34 -0700

    hello example functions

---
+++ hello.js
@@ -0,0 +1,17 @@
+//example 1
+/*
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+//example 2
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
